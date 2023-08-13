14eb2a997d010e475d9328aede7dc325a2894a4d
Ian Chow <135416722+ianthehamster@users.noreply.github.com>
2023-08-13 22:18:07 +0800

    Update script.js
    To delete button scrolling section

--- Bankist App/script.js
+++ Bankist App/script.js
@@ -263,3 +263,21 @@ btnSort.addEventListener('click', function (e) {
   sorted = !sorted;
 });
 
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
